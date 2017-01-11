d660c581bfb51b3f9e7ef10f79faa892045c1499
GabrielS5 <gabriel_schitcu@yahoo.com>
2017-01-11 20:22:03 +0200

    Final Commit
    Pentru a juca trebuie introdus un nume. Am adaugat o tabela de scor care
    poate fi accesata din meniul SFML (se apasa scoruri, apoi unul de
    gamemode-uri pentru a afisa in consola tabela de scor). Daca player-ul
    castiga scorul s-au va fi adaugat automat la tabela potrivita. Scorul
    este timpul terminarii jocului in primele 3 gamemode-uri, iar in mode-ul
    custom scorul ia in considerare numarul de celule, numarul de mine, si
    timpul de rezolvare.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,10 @@
 
 #include <SFML/Graphics.hpp>
 #include<iostream>
-#include <string>
+#include <string.h>
 #include <cstdlib>
 #include <ctime>
+#include <fstream>
 using namespace std;
 int joculContinua=1;
 struct MineSweeper{
@@ -13,7 +14,55 @@ int numarMine;
 int terenMinat[40][40];
 int alegeriJucator[40][40];
 char interfata[40][40];
+char numeJucator[25];
+float scorJucator;
+int tipDeJoc;
 };
+struct scor{
+int numere[40];
+char nume[40][25];
+};
+
+void afisareScoruri(scor scoruri,int optiune)
+{for(int i=0;i<30;i++)
+cout<<endl;
+    for(int i=optiune*10;i<optiune*10+10;i++)
+           if(scoruri.numere[i]>0)
+           {if(i<optiune*10+9)
+            cout<<i%10+1<<" -  Scor "<<scoruri.numere[i]<<" - "<<scoruri.nume[i]<<endl;
+           else
+            cout<<i%10+1<<"-  Scor "<<scoruri.numere[i]<<" - "<<scoruri.nume[i]<<endl;}}
+
+void sortare(int v[11],char w[11][25])
+{int numarAux,minim;
+char sirAux[25];
+for(int i=0;i<11;i++)
+{for(int j=i;j<11;j++)
+{if((v[i]>v[j] && v[j]>0) || v[i]==0)
+  {numarAux=v[i];
+  v[i]=v[j];
+  v[j]=numarAux;
+  strcpy(sirAux,w[i]);
+  strcpy(w[i],w[j]);
+  strcpy(w[j],sirAux);}}}}
+
+void sortareScor(scor &scoruri,MineSweeper Mine,int setscor)
+    {int v[11],k=0;
+    char w[11][25];
+    v[10]=Mine.scorJucator;
+    strcpy(w[10],Mine.numeJucator);
+    for(int i=setscor*10;i<setscor*10+10;i++)
+    {v[k]=scoruri.numere[i];
+    strcpy(w[k],scoruri.nume[i]);
+    k++;}
+    sortare(v,w);
+    k=0;
+    for(int i=setscor*10;i<setscor*10+10;i++)
+    {scoruri.numere[i]=v[k];
+    strcpy(scoruri.nume[i],w[k]);
+    k++;}
+    }
+
 
 void initializareInterfata(char matrice[40][40],int linii,int coloane,char caracter)
 {for(int i=0;i<linii;i++)
@@ -143,7 +192,7 @@ int updateGrafica(MineSweeper &Mine,sf::RectangleShape celulaTablou[40][40],sf::
 {int linia,coloana,poInaltime=0,poLatime=0,laturapatrat=30;
     for(linia=0;linia<Mine.linii;linia++)
     {for(coloana=0;coloana<Mine.coloane;coloana++)
-      {initializareCelulaTablou(celulaTablou[linia][coloana],texturi[19],laturapatrat,poLatime,poInaltime);
+      {initializareCelulaTablou(celulaTablou[linia][coloana],texturi[20],laturapatrat,poLatime,poInaltime);
        if(Mine.interfata[linia][coloana]=='N')
        celulaTablou[linia][coloana].setFillColor (sf::Color(139,213,123));
        else if(Mine.interfata[linia][coloana]=='B')
@@ -177,7 +226,7 @@ return 0;}
 void afisareVictorie(MineSweeper Mine,sf::RectangleShape &celulaTablou,sf::Texture texturi[21],int laturaPatrat)
 {celulaTablou.setSize(sf::Vector2f(150, 75));
 celulaTablou.setFillColor (sf::Color::White);
-celulaTablou.setTexture(&texturi[17]);
+celulaTablou.setTexture(&texturi[18]);
 celulaTablou.setTextureRect(sf::IntRect(0, 0, 500, 250));
 celulaTablou.setPosition((laturaPatrat*Mine.coloane)/2-75,(laturaPatrat*Mine.linii)/2-37);
 celulaTablou.setOutlineColor(sf::Color::Red);
@@ -186,7 +235,7 @@ celulaTablou.setOutlineThickness(-2);}
 void afisareInfrangere(MineSweeper Mine,sf::RectangleShape &celulaTablou,sf::Texture texturi[21],int laturaPatrat)
 {celulaTablou.setSize(sf::Vector2f(150, 75));
 celulaTablou.setFillColor (sf::Color::White);
-celulaTablou.setTexture(&texturi[18]);
+celulaTablou.setTexture(&texturi[19]);
 celulaTablou.setTextureRect(sf::IntRect(0, 0, 500, 250));
 celulaTablou.setPosition((laturaPatrat*Mine.coloane)/2-75,(laturaPatrat*Mine.linii)/2-37);
 celulaTablou.setOutlineColor(sf::Color::Red);
@@ -229,14 +278,18 @@ int interactiuneCuJucatorulMouse(int &coordonataEcranX,int &coordonataEcranY,sf:
            return 0;}
 
 
-void minesweeper(MineSweeper &Mine,sf::RectangleShape celulaTablou[40][40],sf::Texture texturi[21])
+
+void minesweeper(MineSweeper &Mine,sf::RectangleShape celulaTablou[40][40],sf::Texture texturi[21],scor &scoruri)
 {int laturaPatrat,minaLovita=0,coordonataEcranX=0,coordonataEcranY=0,victorie=0, coloana=0,tastaApasata=0,linia=0,steagIndepartat=0;
+int secunda=0;
 laturaPatrat=updateGrafica(Mine,celulaTablou,texturi);
 
 sf::RenderWindow win(sf::VideoMode(laturaPatrat*Mine.coloane,laturaPatrat*Mine.linii), "Minesweeper");
 win.setFramerateLimit(60);
 while (win.isOpen())
 {win.clear();
+if(victorie==0)
+secunda++;
   for(int i=0;i<Mine.linii;i++)
    for(int j=0;j<Mine.coloane;j++)
     win.draw(celulaTablou[i][j]);
@@ -271,12 +324,19 @@ while (win.pollEvent(eveniment))
 
        updateGrafica(Mine,celulaTablou,texturi);
        tastaApasata=0;}}
+       if(victorie==1)
+      {Mine.scorJucator=secunda/60;
+      if(Mine.tipDeJoc==3)
+        Mine.scorJucator=((Mine.linii*Mine.coloane)/Mine.numarMine/2)*Mine.scorJucator;
+      cout<<endl<<endl<<"Scorul tau  "<<Mine.scorJucator;
+      sortareScor(scoruri,Mine,Mine.tipDeJoc);}
 }
 
-void administrareValoriColoaneLiniiMine(MineSweeper &Mine,int mine, int coloane,int linii)
+void administrareValoriColoaneLiniiMine(MineSweeper &Mine,int mine, int coloane,int linii,int tipJoc)
 {Mine.numarMine=mine;
 Mine.linii=linii;
-Mine.coloane=coloane;}
+Mine.coloane=coloane;
+Mine.tipDeJoc=tipJoc;}
 
 void initializareCelulaMeniu(sf::RectangleShape &celuleMeniu,sf::Texture &texturi,int poLatime,int poInaltime)
 {celuleMeniu.setSize(sf::Vector2f(100, 50));
@@ -313,12 +373,15 @@ void interactiuneCuJucatorulTastaturaMeniu(int &numarIntrodus,sf::Event evenimen
 
      }}}
 
-void meniuJocSFML(MineSweeper &Mine,sf::Texture texturi[21])
-{int latime=650,meniuUrmator=0,numarIntrodus=-1,inaltime=300,nrPatrate=4,tastaApasata=0,coloana,linia,terminat=0,folosireTastatura=0;
-  int coordonataEcranX=0,coordonataEcranY=0;
+void meniuJocSFML(MineSweeper &Mine,sf::Texture texturi[21],scor scoruri)
+{int latime=650,meniuUrmator=0,numarIntrodus=-1,inaltime=300,nrPatrate=5,tastaApasata=0,coloana,linia,terminat=0,folosireTastatura=0;
+  int coordonataEcranX=0,coordonataEcranY=0,optiuneScor=0;
     sf::RenderWindow win(sf::VideoMode(latime, inaltime), "Meniu");
 sf::RectangleShape celuleMeniu[5];
-for(int i=0;i<4;i++)
+for(int i=0;i<5;i++)
+if(i==4)
+initializareCelulaMeniu(celuleMeniu[i],texturi[i+10],50,200);
+else
 initializareCelulaMeniu(celuleMeniu[i],texturi[i+10],50+150*i,100);
 while (win.isOpen())
 {win.clear();
@@ -336,7 +399,7 @@ sf::Event eveniment;
      interactiuneCuJucatorulMouse(coloana,linia,eveniment,tastaApasata);
         }
     if(meniuUrmator>=0 && folosireTastatura)
-      initializareCelulaMeniu(celuleMeniu[0],texturi[14+meniuUrmator],50,100);
+      initializareCelulaMeniu(celuleMeniu[0],texturi[15+meniuUrmator],50,100);
     if(numarIntrodus>=0)
       {if(meniuUrmator==0)
         { Mine.coloane=Mine.coloane*10+numarIntrodus;
@@ -350,18 +413,40 @@ sf::Event eveniment;
     if(tastaApasata)
     {  if(linia>100 && linia<150)
          {if(coloana>50 && coloana<150)
-           {administrareValoriColoaneLiniiMine(Mine,10,9,9);
+           if(optiuneScor)
+           {afisareScoruri(scoruri,0);
+            nrPatrate=5;}
+           else
+            {administrareValoriColoaneLiniiMine(Mine,10,9,9,0);
             terminat=1;}
           if(coloana>200 && coloana<300)
-            {administrareValoriColoaneLiniiMine(Mine,40,16,16);
+            if(optiuneScor)
+           {afisareScoruri(scoruri,1);
+            nrPatrate=5;}
+           else
+             {administrareValoriColoaneLiniiMine(Mine,40,16,16,1);
              terminat=1;}
           if(coloana>350 && coloana<450)
-             {administrareValoriColoaneLiniiMine(Mine,99,30,16);
+             if(optiuneScor)
+           {afisareScoruri(scoruri,2);
+            nrPatrate=5;}
+           else
+              {administrareValoriColoaneLiniiMine(Mine,99,30,16,2);
               terminat=1;}
           if(coloana>500 && coloana<600)
-             {nrPatrate=1;
-             initializareCelulaMeniu(celuleMeniu[0],texturi[14],50,100);
-             folosireTastatura=1;}}
+             if(optiuneScor)
+           {afisareScoruri(scoruri,3);
+            nrPatrate=5;}
+           else
+             {Mine.tipDeJoc=3;
+             nrPatrate=1;
+             initializareCelulaMeniu(celuleMeniu[0],texturi[15],50,100);
+             folosireTastatura=1;}
+        optiuneScor=0;
+        }
+        if(linia>200 && linia<250 && coloana>50 && coloana<150)
+        {optiuneScor=1;
+        nrPatrate=4;}
         tastaApasata=0;
     }}}
 
@@ -382,30 +467,68 @@ void incarcareTexturi(sf::Texture texturi[21])
     texturi[11].loadFromFile("Texturi\\Mediu.jpg");
     texturi[12].loadFromFile("Texturi\\Expert.jpg");
     texturi[13].loadFromFile("Texturi\\Custom.jpg");
-    texturi[14].loadFromFile("Texturi\\Col.jpg");
-    texturi[15].loadFromFile("Texturi\\Linii.jpg");
-    texturi[16].loadFromFile("Texturi\\NrMine.jpg");
-    texturi[17].loadFromFile("Texturi\\Victorie.jpg");
-    texturi[18].loadFromFile("Texturi\\Infrangere.jpg");
-    texturi[19].loadFromFile("Texturi\\alb.jpg");
+    texturi[14].loadFromFile("Texturi\\Scoruri.jpg");
+    texturi[15].loadFromFile("Texturi\\Col.jpg");
+    texturi[16].loadFromFile("Texturi\\Linii.jpg");
+    texturi[17].loadFromFile("Texturi\\NrMine.jpg");
+    texturi[18].loadFromFile("Texturi\\Victorie.jpg");
+    texturi[19].loadFromFile("Texturi\\Infrangere.jpg");
+    texturi[20].loadFromFile("Texturi\\alb.jpg");
 }
 
+int citireDinFisier(scor &scoruri)
+{int i=0,j=0,k=0;
+string linie;
+ifstream iesire("scoruri.txt");
+for(int i=0;i<40;i++)
+        scoruri.numere[i]=0;
+while ( getline (iesire,linie) )
+    {i=0;
+      while(linie[i]>=48 && linie[i]<=57)
+        {scoruri.numere[j]=scoruri.numere[j]*10+linie[i]-48;
+        i++;}
+        i++;
+        k=0;
+      while(linie[i]>=64 && linie[i]<=122 || linie[i]==32)
+      {scoruri.nume[j][k]=linie[i];
+        i++;
+        k++;}
+        scoruri.nume[j][k]='\0';
+        j++;
+    }
+    iesire.close();
+    return j;}
 
 int main()
-{
-    MineSweeper Mine;
+{scor scoruri;
+
+citireDinFisier(scoruri);
+MineSweeper Mine;
+Mine.scorJucator=0;
+cin.get(Mine.numeJucator,24);
 sf::RectangleShape celulaTablou[40][40];
 sf::Texture texturi[21];
 srand(time(NULL));
 incarcareTexturi(texturi);
+
 while(joculContinua)
-{meniuJocSFML(Mine,texturi);
-initializareMatrice(Mine.alegeriJucator,Mine.linii,Mine.coloane,0);
-initializareMatrice(Mine.terenMinat,Mine.linii,Mine.coloane,0);
-initializareInterfata(Mine.interfata,Mine.linii,Mine.coloane,'N');
-plantareMine(Mine);
-activareMine(Mine);
-if(joculContinua)
-minesweeper(Mine,celulaTablou,texturi);}
+ {Mine.linii=Mine.coloane=Mine.numarMine=0;
+ meniuJocSFML(Mine,texturi,scoruri);
+ initializareMatrice(Mine.alegeriJucator,Mine.linii,Mine.coloane,0);
+ initializareMatrice(Mine.terenMinat,Mine.linii,Mine.coloane,0);
+ initializareInterfata(Mine.interfata,Mine.linii,Mine.coloane,'N');
+ plantareMine(Mine);
+ activareMine(Mine);
+ if(joculContinua)
+  minesweeper(Mine,celulaTablou,texturi,scoruri);}
+ofstream intrare("scoruri.txt");
+for(int i=0;i<40;i++)
+{intrare<<scoruri.numere[i]<<" ";
+if(scoruri.numere[i]==0)
+intrare<<endl;
+else
+intrare<<scoruri.nume[i]<<endl;}
+intrare.close();
+
     return 0;
 }
