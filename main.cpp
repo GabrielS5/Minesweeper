e75a2857c3c21433d9709bae7c949a2343e2c5af
GabrielS5 <gabriel_schitcu@yahoo.com>
2017-01-11 19:39:53 +0200

    Apopierea Minelor
    Am parcurs matricea terenMinat, iar pentru fiecare mina am incrementat
    celulele din jurul ei.

--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-
 #include <iostream>
 #include <string>
 #include <cstdlib>
@@ -21,7 +20,7 @@ void afisare(int matrice[20][20],int coloaneLinii)
 }
 
 int contineMina(int matrice[20][20],int x,int y)
-{if(matrice[x][y]==0)
+{if(matrice[y][x]==0)
 return 0;
 return 1;
 
@@ -33,14 +32,54 @@ while(numarMine>0)
 {coordonataX=rand()%coloaneLinii;
 coordonataY=rand()%coloaneLinii;
 if(!contineMina(terenMinat,coordonataX,coordonataY))
-{terenMinat[coordonataX][coordonataY]=-1;
+{terenMinat[coordonataY][coordonataX]=-1;
 numarMine--;}}}
+
+bool verificarePlasareMina(int terenMinat[20][20],int y,int x,int coloaneLinii)
+{if(x>=coloaneLinii || y>=coloaneLinii || x<0 || y<0)
+return 0;
+if(terenMinat[y][x]==-1)
+    return 0;
+return 1;
+
+}
+
+void activareMine(int terenMinat[20][20],int coloaneLinii)
+{int x,y;
+for(y=0;y<coloaneLinii;y++)
+for(x=0;x<coloaneLinii;x++)
+{if(terenMinat[y][x]==-1)
+{if(verificarePlasareMina(terenMinat,y,x+1,coloaneLinii))
+terenMinat[y][x+1]++;
+if(verificarePlasareMina(terenMinat,y,x-1,coloaneLinii))
+terenMinat[y][x-1]++;
+if(verificarePlasareMina(terenMinat,y+1,x,coloaneLinii))
+terenMinat[y+1][x]++;
+if(verificarePlasareMina(terenMinat,y-1,x,coloaneLinii))
+terenMinat[y-1][x]++;
+if(verificarePlasareMina(terenMinat,y-1,x+1,coloaneLinii))
+terenMinat[y-1][x+1]++;
+if(verificarePlasareMina(terenMinat,y+1,x+1,coloaneLinii))
+terenMinat[y+1][x+1]++;
+if(verificarePlasareMina(terenMinat,y-1,x-1,coloaneLinii))
+terenMinat[y-1][x-1]++;
+if(verificarePlasareMina(terenMinat,y+1,x-1,coloaneLinii))
+terenMinat[y+1][x-1]++;}
+}
+
+}
+
+
+
+
 
 int main()
 {srand(time(NULL));
 int terenMinat[20][20];
 initializareMatrice(terenMinat,10);
 plantareMine(terenMinat,10,9);
+afisare(terenMinat,9);
+activareMine(terenMinat,9);
 afisare(terenMinat,9);
 
 }
